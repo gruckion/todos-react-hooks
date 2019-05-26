721144941f44bfcb9c32b2dd81a5d931f6bd5532
Stephen Rayner <stephenrrayner@dev.azure.com>
2019-05-26 18:07:06 +0100

    simple counter with useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer } from "react";
 import { UserContext } from ".";
 
+interface State {
+  count: number;
+}
+
+const initialState: State = {
+  count: 0,
+};
+
+function reducer(state: State, action: any): any {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+}
+
 const App: React.FC = (): JSX.Element => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const username = useContext(UserContext);
 
-  return <div>Hello {username}</div>;
+  return (
+    <div>
+      Hello {username}
+      Count: {state.count}
+      <button className="border p-1 m-1" type="button" onClick={() => dispatch({ type: "INCREMENT" })}>
+        Increment
+      </button>
+      <button className="border p-1 m-1" type="button" onClick={() => dispatch({ type: "DECREMENT" })}>
+        Decrement
+      </button>
+    </div>
+  );
 };
 
 export default App;
