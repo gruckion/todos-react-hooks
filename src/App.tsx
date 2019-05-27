4cff50aee2c21fc90314079b698e62244a586cc9
Stephen Rayner <stephenrrayner@dev.azure.com>
2019-05-27 11:46:12 +0100

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import React, { useContext, useReducer } from "react";
-import { TodosContext } from "./context";
-import { todosReducer } from "./reducer";
-import { TodoList } from "./components/TodoList";
+import React from "react";
 
 export const App = (): JSX.Element => {
-  const initialState = useContext(TodosContext);
-  const [state] = useReducer(todosReducer, initialState);
-
-  return (
-    <TodosContext.Provider value={state}>
-      <TodoList />
-    </TodosContext.Provider>
-  );
+  return <div>hi</div>;
 };
